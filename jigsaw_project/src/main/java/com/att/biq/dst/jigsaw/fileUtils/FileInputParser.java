1ec9186611385f4e3f23edc63c70b90435821ed1
Dorit Rieur <dr9081@att.com>
2018-03-30 12:20:58 +0300

    start parser staff

--- jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/fileUtils/FileInputParser.java
+++ jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/fileUtils/FileInputParser.java
@@ -1,4 +1,65 @@
 package com.att.biq.dst.jigsaw.fileUtils;
 
+import java.util.List;
+
 public class FileInputParser {
+
+
+    /**
+     * get number of elements by realing the first line in the file.
+     * if there is an error on the first line, return -1, and report to file.
+     *
+     * @param list
+     * @return int containing number of elements
+     */
+    public static int getNumberOfElements(List<String> list) {
+
+        String firstLine = list.get(0);
+//if (firstLine.startsWith("NumElements="))
+
+        String[] tempStr = firstLine.split("=");
+        if ((tempStr[0].contains("NumElements") && tempStr[0].contains(" '")) && (tempStr.length == 2)) {
+            try {
+                return Integer.valueOf(tempStr[1]);
+            } catch (NumberFormatException e) {
+                reportError("Number of elements does not indicate number");
+                return -1;
+            }
+        } else {
+            reportError("Either string does not contains the prefix \"NumElements\" or any other error on the first line");
+            return -1;
+        }
+
+    }
+
+    private static void reportError(String s) {
+    }
+
+    public static boolean isLineEmpty() {
+
+
+        return true;
+    }
+
+
+    public static boolean verifyLine() {
+        if (!isLineEmpty() || !isLineContainsOnlySpaces() || !isIDInRange()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isIDInRange() {
+        return true;
+    }
+
+    private static boolean isLineContainsOnlySpaces() {
+
+        return true;
+    }
+
+    private static boolean isLineBeginswithDash() {
+        return true;
+    }
 }
--- jigsaw_project/src/test/com/att/biq/dst/jigsaw/fileUtils/FileInputParserTest.java
+++ jigsaw_project/src/test/com/att/biq/dst/jigsaw/fileUtils/FileInputParserTest.java
@@ -1,5 +1,54 @@
 package com.att.biq.dst.jigsaw.fileUtils;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FileInputParserTest {
 
+    static String basePath = "C:\\git_clone\\bit_std\\jigsaw\\jigsaw_project\\src\\resources\\";
+    File file;
+    FileManager fm;
+
+    @BeforeEach
+    public void setUp() {
+
+        fm = new FileManager();
+
+    }
+
+    @Test
+    public void verifyCorrectFirstLine(){
+        Path path = Paths.get(basePath + "testFileValidFormat.txt");
+        List<String> list = FileManager.readFromFile(path);
+        assertEquals(FileInputParser.getNumberOfElements(list),7);
+    }
+
+
+    @Test
+    public void firstLineParseError(){
+        Path path = Paths.get(basePath + "testFileValidFormat.txt");
+        List<String> list = FileManager.readFromFile(path);
+        assertEquals(FileInputParser.getNumberOfElements(list),-1);
+    }
+
+    @Test
+    public void lineContainsOnlySpaces(){
+
+//        Throwable exception = assertThrows(FileParsingError.class,() ->
+//
+//            );
+//        }
+//
+//    assertEquals(FileInputParser.isLineContainsOnlySpaces(),-1) {
+
+    }
+
 }
+
