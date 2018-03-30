6f83e8e94f8acbc2451ae265e7f59d5a69fc0f02
Dorit Rieur <dr9081@att.com>
2018-03-30 17:25:03 +0300

    file parser +  file parser tests -- first commit

--- jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/fileUtils/FileInputParser.java
+++ jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/fileUtils/FileInputParser.java
@@ -1,9 +1,11 @@
 package com.att.biq.dst.jigsaw.fileUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileInputParser {
 
+    FileManager fm = new FileManager();
 
     /**
      * get number of elements by realing the first line in the file.
@@ -12,54 +14,241 @@ public class FileInputParser {
      * @param list
      * @return int containing number of elements
      */
-    public static int getNumberOfElements(List<String> list) {
+    public int getNumberOfElements(List<String> list) {
 
         String firstLine = list.get(0);
-//if (firstLine.startsWith("NumElements="))
-
         String[] tempStr = firstLine.split("=");
-        if ((tempStr[0].contains("NumElements") && tempStr[0].contains(" '")) && (tempStr.length == 2)) {
+        if ((tempStr[0].contains("NumElements") || tempStr[0].contains(" ")) && (tempStr.length == 2)) {
             try {
                 return Integer.valueOf(tempStr[1]);
             } catch (NumberFormatException e) {
-                reportError("Number of elements does not indicate number");
+                fm.reportError("Number of elements does not indicate number");
                 return -1;
             }
         } else {
-            reportError("Either string does not contains the prefix \"NumElements\" or any other error on the first line");
+            fm.reportError("Either string does not contains the prefix \"NumElements\" or any other error on the first line");
             return -1;
         }
 
     }
 
-    private static void reportError(String s) {
+
+    /**
+     * verify if line on the list is empty - report error and return indication
+     *
+     * @param line
+     * @return
+     */
+    public boolean isLineEmpty(String line) {
+
+        if (line.isEmpty()) {
+
+            return true;
+        } else return false;
     }
 
-    public static boolean isLineEmpty() {
+
+    /**
+     * recieves list of lines from the file, return valid lines for puzzle
+     *
+     * @param list
+     * @return
+     */
+//TODO TESTS
+    public int[][] produceArrayForPuzzle(List<String> list) {
+        int indexLines = 0;
+        List<String> wrongElementIDs = new ArrayList<>();
+        List<String> missingElementsIDs = new ArrayList<>();
+
+        for ( String line : list ) {
+            int countErrors = 0;
+            if (!wrongIDRange(list, line)) {
+                countErrors += 1;
+                fm.reportError("id:  " + getLinePuzzlePieceID(line) + "  in not in the range:  " + getFileRange(list));
+            }
+
+            if (isLineContainsOnlySpaces(line) || isLineEmpty(line)) {
+                countErrors += 1;
+                //TODO should be ignored, should we report it?
+                fm.reportError("lineNumber:  " + indexLines + "  contains only spaces or is empty ");
+
+            }
+
+            if (isLineBeginswithDash(line)) {
+                countErrors += 1;
+                fm.reportError("lineNumber:  " + indexLines + "  begins with dash, ignore ");
+
+            }
+            if (isMissingElementInInputFile()) {
+                countErrors += 1;
+                missingElementsIDs.add(String.valueOf(getElementID(line)));
 
 
-        return true;
+            }
+
+
+            if (isMissingElementInInputFile()) {
+                countErrors += 1;
+                fm.reportError("Missing puzzle element(s) with the following IDs:  " + "");
+
+            }
+
+            if (wrongIDRange(list,  line)) {
+                countErrors += 1;
+                wrongElementIDs.add(String.valueOf(getElementID(line)));
+
+            }
+
+            if (isWrongElementFormat()) {
+                countErrors += 1;
+                fm.reportError("Puzzle ID <" + indexLines + "> as wrong data: <" + getPuzzlePieceData(line));
+
+
+            }
+
+//            Puzzle ID <id> has wrong data: <complete line from file including ID>
+            //check if line is valid
+
+            if (countErrors == 0) {
+                //add to intArray
+            }
+
+            indexLines = +1;
+        }
+
+        fm.reportError("Puzzle of size " + getNumberOfElements(list) + " cannot have the following IDs:" + wrongElementIDs);
+      //TODO sort before report
+        fm.reportError("Puzzle of size " + getNumberOfElements(list) + " cannot have the following IDs:" + missingElementsIDs);
+
+        return new int[][]{{0, 0, 1, -1}, {1, 0, 0, 1}, {0, 1, 1, 0}, {-1, 1, 0, 0}};
     }
 
 
-    public static boolean verifyLine() {
-        if (!isLineEmpty() || !isLineContainsOnlySpaces() || !isIDInRange()) {
+    /**
+     * get the puzzlePiece part for validation
+     *
+     * @param line
+     * @return string contains the puzzle piece
+     */
+    public String getPuzzlePieceData(String line) {
+        String[] lineArr = line.split(",");
+
+        String piece = "";
+        for ( int i = 1; i < lineArr.length; i++ ) {
+
+            piece = piece + lineArr[i] + ",";
+
+        }
+        return piece.substring(0, piece.lastIndexOf(",")).replace(" ", "");
+
+    }
+
+    /**
+     * is the ID in the range of the number of elements expected??
+     *
+     * @param list
+     * @param line
+     * @return true/false
+     */
+    public boolean wrongIDRange(List<String> list, String line) {
+
+        int numberOfElements = getNumberOfElements(list);
+        int puzzlePieceID = getLinePuzzlePieceID(line);
+        if (puzzlePieceID <= numberOfElements) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Return rhe ID as int to create a number
+     *
+     * @param line
+     * @return true/false
+     */
+    public int getElementID(String line) {
+
+
+        return getLinePuzzlePieceID(line);
+
+
+    }
+
+    /**
+     * does this line contains only spaces
+     *
+     * @param line
+     * @return
+     */
+    public boolean isLineContainsOnlySpaces(String line) {
+        if (line.length() == line.chars().filter(ch -> ch == ' ').count()) {
             return true;
         }
+        return false;
+    }
+
+
+
+
+
+
+    /**
+     * get the file ID range
+     * @param list
+     * @return String
+     */
+
+    public String getFileRange(List<String> list) {
+        if (getNumberOfElements(list)==1) return "1";
+        if (getNumberOfElements(list)==(-1)) return "N/A";
+        return "1-" + getNumberOfElements(list);
+    }
+
+    /**
+     * get line puzzle piece ID
+     *
+     * @param line
+     * @return pieceID
+     */
+     public int getLinePuzzlePieceID(String line) {
+        try {
+            String[] lineArr = line.split(",");
+            return Integer.valueOf(lineArr[0]);
+
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+
+    }
+
+    /**
+     * is this line begins with # (should be reported and ignored)
+     * @param line
+     * @return
+     */
+
+    public boolean isLineBeginswithDash(String line) {
+        line = line.replace(" ","");
+
+         if(line.startsWith("#")){
+             return true;
+         }
 
         return false;
     }
 
-    private static boolean isIDInRange() {
-        return true;
-    }
 
-    private static boolean isLineContainsOnlySpaces() {
+    //TODO + TESTS
+    private boolean isMissingElementInInputFile() {
 
         return true;
     }
 
-    private static boolean isLineBeginswithDash() {
+    //TODO + TESTS
+    private boolean isWrongElementFormat() {
+
         return true;
     }
 }
--- jigsaw_project/src/test/com/att/biq/dst/jigsaw/fileUtils/FileInputParserTest.java
+++ jigsaw_project/src/test/com/att/biq/dst/jigsaw/fileUtils/FileInputParserTest.java
@@ -1,53 +1,274 @@
 package com.att.biq.dst.jigsaw.fileUtils;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-class FileInputParserTest {
+public class FileInputParserTest {
 
     static String basePath = "C:\\git_clone\\bit_std\\jigsaw\\jigsaw_project\\src\\resources\\";
-    File file;
-    FileManager fm;
-
-    @BeforeEach
-    public void setUp() {
-
-        fm = new FileManager();
-
-    }
-
-    @Test
-    public void verifyCorrectFirstLine(){
-        Path path = Paths.get(basePath + "testFileValidFormat.txt");
-        List<String> list = FileManager.readFromFile(path);
-        assertEquals(FileInputParser.getNumberOfElements(list),7);
-    }
-
-
-    @Test
-    public void firstLineParseError(){
-        Path path = Paths.get(basePath + "testFileValidFormat.txt");
-        List<String> list = FileManager.readFromFile(path);
-        assertEquals(FileInputParser.getNumberOfElements(list),-1);
-    }
-
-    @Test
-    public void lineContainsOnlySpaces(){
-
-//        Throwable exception = assertThrows(FileParsingError.class,() ->
+//    File file;
+//    FileManager fm = new FileManager();
+    FileInputParser fip = new FileInputParser();
 //
-//            );
+//    @BeforeEach
+//    public void setUp() {
+//
+//        fm = new FileManager();
+//
+//    }
+
+    // *************************   Verify first line Tests
+    @Test
+    public void verifyCorrectFirstLine() {
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=4");
+
+        assertEquals(4, fip.getNumberOfElements(list));
+    }
+
+    @Test
+    public void missingNumnElements() {
+//        Path path = Paths.get(basePath + "testFileValidFormat.txt");
+        List<String> list = new ArrayList<>();
+        list.add("NElements=4");
+
+        assertEquals(-1, fip.getNumberOfElements(list));
+    }
+
+    @Test
+    public void missingInteger() {
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=");
+
+        assertEquals(-1, fip.getNumberOfElements(list));
+    }
+
+
+    @Test
+    public void notIntegerOnNumberOfElements() {
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=o");
+
+        assertEquals(-1, fip.getNumberOfElements(list));
+    }
+
+    @Test
+    public void spacesBeforeNumElements() {
+        List<String> list = new ArrayList<>();
+        list.add("   NumElements=77");
+
+        assertEquals(77, fip.getNumberOfElements(list));
+    }
+
+
+    @Test
+    public void spacesAfterNumElements() {
+        List<String> list = new ArrayList<>();
+        list.add("NumElements    =77");
+
+        assertEquals(77, fip.getNumberOfElements(list));
+    }
+
+    @Test
+    public void firstLineParseError() {
+        Path path = Paths.get(basePath + "testFileValidFormat.txt");
+        List<String> list = FileManager.readFromFile(path);
+        assertEquals(fip.getNumberOfElements(list), 4);
+    }
+
+    // *************************   Verify line contains only spaces
+    @Test
+    public void lineContainsOnlySpaces() {
+
+        List<String> list = new ArrayList<>();
+        list.add(" ");
+
+        assertTrue(fip.isLineContainsOnlySpaces(list.get(0)));
+
+    }
+
+    @Test
+    public void lineContainsAlsoSpaces() {
+
+        List<String> list = new ArrayList<>();
+        list.add("1, 0,-1 ,9");
+
+        assertFalse(fip.isLineContainsOnlySpaces(list.get(0)));
+
+    }
+
+    // *************************   Verify line is empty
+    @Test
+    public void lineIsEmptyNegative(){
+
+        List<String> list = new ArrayList<>();
+        list.add("34, 1, 0,-1 ,9");
+
+        assertFalse(fip.isLineEmpty(list.get(0)));
+
+
+    }
+
+    @Test
+    public void lineIsEmpty(){
+
+        List<String> list = new ArrayList<>();
+        list.add("");
+
+        assertTrue(fip.isLineEmpty(list.get(0)));
+
+    }
+
+
+    @Test
+    public void lineIsEmptyOneSpace(){
+
+        List<String> list = new ArrayList<>();
+        list.add(" ");
+
+        assertFalse(fip.isLineEmpty(list.get(0)));
+
+    }
+
+
+    // *************************   Verify line wrongIDRange
+
+
+    @Test
+    public void IDInRange(){
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=100");
+        list.add("34, 1, 0,-1 ,1,1");
+        list.add("24, 1, 0,-1 ,1,1");
+        list.add("100, 1, 0,-1 ,1,1");
+        list.add("101, 1, 0,-1 ,1,1");
+        list.add("109, 1, 0,-1 ,1,1");
+
+assertTrue(fip.wrongIDRange(list,list.get(1)));
+        assertTrue(fip.wrongIDRange(list,list.get(2)));
+        assertTrue(fip.wrongIDRange(list,list.get(3)));
+        assertFalse(fip.wrongIDRange(list,list.get(4)));
+        assertFalse(fip.wrongIDRange(list,list.get(5)));
+
+
+    }
+
+// *************************   Verify getLinePuzzlePieceID
+@Test
+
+    public void linePuzzlePieceIDValid(){
+    List<String> list = new ArrayList<>();
+    list.add("NumElements=100");
+    list.add("34, 1, 0,-1 ,1,1");
+    assertEquals(34,fip.getLinePuzzlePieceID(list.get(1)));
+}
+
+
+    @Test
+
+    public void linePuzzlePieceIDNotInteger(){
+
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=100");
+        list.add("aa, 1, 0,-1 ,1,1");
+        assertEquals(-1,fip.getLinePuzzlePieceID(list.get(1)));
+    }
+
+// *************************   Verify getLinePuzzlePieceID
+//getPuzzlePieceData(String line)
+
+    @Test
+    public void getValidPuzzlePiece(){
+
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=100");
+        list.add("5,0,-1,1,1");
+        assertEquals("0,-1,1,1",fip.getPuzzlePieceData(list.get(1)));
+    }
+
+
+    @Test
+    public void getValidPuzzlePieceWithSpaces(){
+
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=100");
+        list.add("5, 0,-1 ,1,1");
+        assertEquals("0,-1,1,1",fip.getPuzzlePieceData(list.get(1)));
+    }
+
+// *************************   Verify getFileRange
+@Test
+    public void getFileRangeValid(){
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=100");
+        assertEquals("1-100",fip.getFileRange(list));
+    }
+
+
+    @Test
+    public void getFileRangeOneRange(){
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=1");
+        assertEquals("1",fip.getFileRange(list));
+    }
+
+
+    @Test
+    public void getFileRangeNonValid(){
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=t");
+        assertEquals("N/A",fip.getFileRange(list));
+    }
+
+    // *************************   Verify isLineBeginswithDash
+//    private boolean isLineBeginswithDash(String line) {
+//        line.replace(" ","");
+//        if(line.startsWith("#")){
+//            return true;
 //        }
 //
-//    assertEquals(FileInputParser.isLineContainsOnlySpaces(),-1) {
+//        return false;
+//    }
 
+    @Test
+    public void lineBeginsWithDash(){
+        List<String> list = new ArrayList<>();
+        list.add("#NumElements=t");
+        assertTrue(fip.isLineBeginswithDash(list.get(0)));
+        list.add("#5, 0,-1 ,1,1");
+        assertTrue(fip.isLineBeginswithDash(list.get(1)));
+    }
+
+    @Test
+    public void lineBeginsWithDash_ignoreSpaces(){
+
+        List<String> list = new ArrayList<>();
+        list.add("   #NumElements=t");
+        assertTrue(fip.isLineBeginswithDash(list.get(0)));
+    }
+
+
+
+    @Test
+    public void lineBeginsWithNoDash(){
+
+        List<String> list = new ArrayList<>();
+        list.add("NumElements=t");
+        assertFalse(fip.isLineBeginswithDash(list.get(0)));
+    }
+
+    @Test
+    public void lineBeginsWithNoDashWithSpaces(){
+
+        List<String> list = new ArrayList<>();
+        list.add("  NumElements=t");
+        assertFalse(fip.isLineBeginswithDash(list.get(0)));
     }
 
 }
