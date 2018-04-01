e69dfc1f058008c53d06861a64745dd34519e029
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-04-01 17:44:27 +0300

    Updating FileInputParser and FileInputParserTest classes

--- jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/fileUtils/FileInputParser.java
+++ jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/fileUtils/FileInputParser.java
@@ -1,4 +1,22 @@
-package com.att.biq.dst.jigsaw.fileUtils;
+package File;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FileInputParser {
+
+
+    public static boolean validateIds(Integer[][] text) {
+
+        Set<Integer> ids = new HashSet<>();
+
+        for (int i=0;i<text.length;i++){
+            ids.add(text[i][0]);
+        }
+        boolean result = true;
+        if (ids.size()!=text.length || Collections.min(ids)!=1 || Collections.max(ids)!= text.length)result=false;
+
+        return result;
+    }
 }
--- jigsaw_project/src/test/com/att/biq/dst/jigsaw/fileUtils/FileInputParserTest.java
+++ jigsaw_project/src/test/com/att/biq/dst/jigsaw/fileUtils/FileInputParserTest.java
@@ -1,5 +1,26 @@
-package com.att.biq.dst.jigsaw.fileUtils;
+package File;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class FileInputParserTest {
+
+    @Test
+    public void positiveTestParserValidateIds(){
+        Assertions.assertTrue(File.FileInputParser.validateIds(new Integer[][]{{1},{2}, {3}, {4}}), "ID validation passed unexpectedly");
+    }
+
+
+    @Test
+    public void testParserValidateIdsWithMissingId(){
+        Assertions.assertFalse(File.FileInputParser.validateIds(new Integer[][]{{1}, {3}, {4}}), "ID validation failed unexpectedly");
+    }
+
+    @Test
+    public void testParserValidateIdsWithUnOrderedIds(){
+        Assertions.assertTrue(File.FileInputParser.validateIds(new Integer[][]{{1}, {3}, {2},{4}}), "ID validation failed unexpectedly");
+    }
+
+
 
 }
