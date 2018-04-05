c3f4cf476d26164194d79fa6771d9b03ec519d06
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-04-05 07:15:07 +0300

    Add getPuzzle method, which uses other methods to read and validate the puzzle. Method produces a list of int[] which holds the puzzle pieces data

--- jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/puzzleManager/Puzzle.java
+++ jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/puzzleManager/Puzzle.java
@@ -1,17 +1,42 @@
 package com.att.biq.dst.jigsaw.puzzleManager;
 
+import com.att.biq.dst.jigsaw.fileUtils.FileInputParser;
 import com.att.biq.dst.jigsaw.fileUtils.FileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Puzzle {
 
-    static FileManager fileManager = new FileManager();
+
     private PuzzleSolution solution;
 
     public static void main(String[] args) {
+        String inputFilePath = "c:/puzzle/puzzle.txt";
+        FileManager fm = new FileManager("c:/puzzle/output_");
+        List<int[]> puzzleDataFromFile = getPuzzle(inputFilePath, fm);
 
 
+    }
 
-
-
+    /**
+     *
+     * @param filePath
+     * @param fileManager
+     * @return
+     */
+    public static ArrayList<int[]> getPuzzle(String filePath, FileManager fileManager){
+        Path inputFilePath = Paths.get(filePath);
+        FileInputParser fileInputParser = new FileInputParser();
+        ArrayList<int[]> puzzle =fileInputParser.produceArrayForPuzzle(FileManager.readFromFile(inputFilePath), fileManager);
+        if (puzzle==null || !PuzzlePieceValidators.validatePuzzle(puzzle, fileManager)){
+            for(String error: fileManager.getErrorReportList()){
+                fileManager.writeToFile(error);
+            }
+            throw new RuntimeException("Puzzle input is invalid");
+        }
+        return puzzle;
     }
 }
