f4ec1779992e0e1429a27604614d55c644ac78c5
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-04-27 11:39:46 +0300

    Add reading of numThreads and creating ThreadsManager

--- jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/puzzle/Main.java
+++ jigsaw_project/src/main/java/com/att/biq/dst/jigsaw/puzzle/Main.java
@@ -1,13 +1,19 @@
 package com.att.biq.dst.jigsaw.puzzle;
 
+import com.att.biq.dst.jigsaw.PuzzleUtils.ThreadsManager;
+
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-                PuzzleManager puzzleManager = new PuzzleManager(args[0],args[1]);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ThreadsManager threadsManager;
+        PuzzleManager puzzleManager = new PuzzleManager("./src/main/resources/input/AdvancedPuzzleTests/Input/test15.in","c:\\puzzle\\");
+                if (args.length>=4 &&args[3]!=null){
+                    threadsManager = new ThreadsManager(Integer.valueOf(args[3]));
+                }else
+                    {threadsManager = new ThreadsManager(4);}
                 puzzleManager.loadPuzzle();
-                puzzleManager.playPuzzle();
+                puzzleManager.playPuzzle(threadsManager);
 
             }
 }
