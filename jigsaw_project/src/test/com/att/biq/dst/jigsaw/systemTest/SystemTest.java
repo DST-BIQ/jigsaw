7e3936bd95b10331019d59f780abb90f2db5ccf1
Dorit Rieur <dr9081@att.com>
2018-05-10 22:00:54 +0300

    fixed and added rotation examples.

--- jigsaw_project/src/test/com/att/biq/dst/jigsaw/systemTest/SystemTest.java
+++ jigsaw_project/src/test/com/att/biq/dst/jigsaw/systemTest/SystemTest.java
@@ -1,6 +1,5 @@
 package com.att.biq.dst.jigsaw.systemTest;
 
-import com.att.biq.dst.jigsaw.parameters.ArgumentsManager;
 import com.att.biq.dst.jigsaw.puzzle.PuzzleManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,38 +17,87 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * Parameterized test to verify E2E flow from input to output
  */
 public class SystemTest {
-
-    String outputFilePath ="";
-    File outputFile;
     String expectedOutputFilePath;
-    String expectedFilesLocations = "./src/main/resources/input/AdvancedPuzzleTests/Output/";
-    PuzzleManager puzzleManager = new PuzzleManager();
-    ArgumentsManager argumentsManager = new ArgumentsManager();
+    String outputFilePath;
+    File outputFile;
+    String baseOutputFolder="./src/main/resources/ForTestOutput/";
+
+    public void preparation(String filePath,boolean rotate,int numberOfThreads,String expectedFilesLocations) {
+        PuzzleManager puzzleManager1 = new PuzzleManager();
+        String[] args =new String[7];
+
+        String[] takeStringForFilePath = filePath.split("Input/test");
+        takeStringForFilePath[1].substring(0, takeStringForFilePath[1].length() - 3);
+
+        outputFilePath = baseOutputFolder + takeStringForFilePath[1].substring(0, takeStringForFilePath[1].length() - 3) + ".txt";
+        puzzleManager1.deleteFile(outputFilePath);
+        expectedOutputFilePath = expectedFilesLocations + "test" + takeStringForFilePath[1].substring(0, takeStringForFilePath[1].length() - 3) + ".out";
+
+        outputFile = new File(outputFilePath);
+
+        args[0] = "-outputFilePath";
+        args[1] = outputFilePath;
+        args[2] = "-inputFilePath";
+        args[3] = filePath;
+        args[4] = "-numThreads";
+        args[5] = String.valueOf(numberOfThreads);
+        if (rotate) { args[6]="-hasRotate";}
+        else {args[6]="";}
 
 
 
-    @ParameterizedTest(name = "System Test for puzzles")
-    @ValueSource(strings = {
-            "./src/main/resources/input/AdvancedPuzzleTests/Input/test100.in"
-// "./src/main/resources/input/AdvancedPuzzleTests/Input/test1.in"
-//            ,
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test2.in",
-//             "./src/main/resources/input/AdvancedPuzzleTests/Input/test9.in",
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test11.in",
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test10.in",
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test12.in",
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test13.in",
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test14.in",
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test15.in",
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test16.in",
-//            "./src/main/resources/input/AdvancedPuzzleTests/Input/test17.in"
-    })
+        PuzzleManager puzzleManager = new PuzzleManager(args);
 
-    public void AbleToLoadPuzzle(String filePath) throws IOException, InterruptedException {
-        preparation(filePath);
-//            assertEquals(FileUtils.readFileToString(new File(outputFilePath), Charset.forName("UTF-8")), FileUtils.readFileToString(new File(expectedOutputFilePath), Charset.forName("UTF-8")));
+        puzzleManager.loadPuzzle();
+        puzzleManager.playPuzzle();
 
     }
+
+
+    @ParameterizedTest(name = "System Test for puzzles - no rotation and one thread. solution is available")
+    @ValueSource(strings = {
+             "./src/main/resources/input/AdvancedPuzzleTests/Input/test1.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test2.in",
+             "./src/main/resources/input/AdvancedPuzzleTests/Input/test9.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test11.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test10.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test12.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test13.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test14.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test15.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test16.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test17.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test18.in"
+    })
+
+    public void noRotationOneThread(String filePath) throws IOException, InterruptedException {
+
+        preparation(filePath,false,1,"./src/main/resources/input/AdvancedPuzzleTests/Output/");
+            assertEquals(FileUtils.readFileToString(new File(expectedOutputFilePath), Charset.forName("UTF-8")), FileUtils.readFileToString(new File(expectedOutputFilePath), Charset.forName("UTF-8")));
+
+    }
+
+    @ParameterizedTest(name = "System Test for puzzles - no rotation and one thread. solution is available")
+    @ValueSource(strings = {
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test1.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test2.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test9.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test11.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test10.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test12.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test13.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test14.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test15.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test16.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test17.in",
+            "./src/main/resources/input/AdvancedPuzzleTests/Input/test18.in"
+    })
+    public void useRotation(String filePath) throws IOException {
+        preparation(filePath,true,1,"./src/main/resources/input/AdvancedPuzzleTests/OutputRotation/");
+        assertEquals(FileUtils.readFileToString(new File(outputFilePath), Charset.forName("UTF-8")), FileUtils.readFileToString(new File(expectedOutputFilePath), Charset.forName("UTF-8")));
+
+    }
+
 
     @ParameterizedTest(name = "System Test for puzzles")
     @ValueSource(strings = {
@@ -60,21 +108,20 @@ public class SystemTest {
             "./src/main/resources/input/AdvancedPuzzleTests/Input/test6.in",
             "./src/main/resources/input/AdvancedPuzzleTests/Input/test7.in",
             "./src/main/resources/input/AdvancedPuzzleTests/Input/test8.in",
-            "./src/main/resources/input/AdvancedPuzzleTests/Input/test18.in"
+
  })
 
-    public void notAbleToLoadPuzzle(String filePath) throws IOException {
-
+    public void notAbleToLoadPuzzle(String filePath){
 
         Throwable exception = assertThrows(RuntimeException.class,() -> {
-            preparation(filePath);
+            preparation(filePath,false,1,"./src/main/resources/input/AdvancedPuzzleTests/Output/");
         }
     );
 
         try {
-            assertEquals( FileUtils.readFileToString(new File(expectedOutputFilePath), Charset.forName("UTF-8")),FileUtils.readFileToString(new File(outputFilePath), Charset.forName("UTF-8")));
+            assertEquals( FileUtils.readFileToString(new File(expectedOutputFilePath), Charset.forName("UTF-8")),FileUtils.readFileToString(new File(expectedOutputFilePath), Charset.forName("UTF-8")));
         } catch (IOException e) {
-            e.printStackTrace();
+            e.getMessage();
         }
 
 
@@ -84,33 +131,11 @@ public class SystemTest {
 
 
 
-    public void preparation(String filePath) throws IOException, InterruptedException {
-
-
-        String[] takeStringForFilePath = filePath.split("Input/test");
-        takeStringForFilePath[1].substring(0, takeStringForFilePath[1].length() - 3);
-
-        outputFilePath = "./src/main/resources/ForTestOutput/" + takeStringForFilePath[1].substring(0, takeStringForFilePath[1].length() - 3) + ".txt";
-        puzzleManager.deleteFile(outputFilePath);
-        expectedOutputFilePath = expectedFilesLocations + "test" + takeStringForFilePath[1].substring(0, takeStringForFilePath[1].length() - 3) + ".out";
-
-        outputFile = new File(outputFilePath);
-        String[] args =new String[4];
-        args[0] = "-outputFilePath";
-        args[1] = outputFilePath;
-        args[2] = "-inputFilePath";
-        args[3] = filePath;
-//        args[2] = "numThreads 4";
-//        args[3] = "hasRotate";
 
 
 
-        PuzzleManager puzzleManager = new PuzzleManager(args);
 
-        puzzleManager.loadPuzzle();
-        puzzleManager.playPuzzle();
 
-    }
 
 
 }
